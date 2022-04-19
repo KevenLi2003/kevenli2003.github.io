a8905b08042c7d9c1f17c5474d7f0a90fb2437ee
KevenLi2003 <96700363+KevenLi2003@users.noreply.github.com>
2022-04-18 21:43:15 -0700

    Improvements to the site
    1. Some price tags added
    2. Minor bugs fixed

--- login/login-method.js
+++ login/login-method.js
@@ -5,7 +5,7 @@ function comparePassword() {
     let userPasswordInput = document.getElementById("userPassword").value;
     let correctPassword = 618618;
     if (userEmailInput == correctEmail && userPasswordInput == correctPassword) {
-        window.location.href = '../travel-package-1/index.html';
+        history.back();
         let signInState = true;
     } else if (userEmailInput != correctEmail && userPasswordInput == correctPassword) {
             window.alert('你输入的邮箱尚未注册!');
